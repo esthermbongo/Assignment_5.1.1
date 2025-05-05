95b8c2c47e17a4763d691be0fe2b7be5f1e3928a
icook <icook@LAPTOP-NCUV50HH>
2025-05-05 18:46:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment_5._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int reverse = 0;
+
+            bool result = IfPalindrome(x);
+            Console.WriteLine(result);
+        }
+
+        public static bool IfPalindrome(int x)
+        {
+            while (x > 0)
+            {
+               x = x % 10;
+            }
+
+        }
+
+    }
+}
